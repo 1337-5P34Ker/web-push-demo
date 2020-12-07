2a8c0890e5769e21f078a1f71a0c766b02c4bf81
Rocco Hundertmark <rocco@ronami.de>
2020-12-07 22:48:52 +0100

    client with index.html

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
+// Path to localhost
+app.use(express.static(path.join(__dirname, "client")));
 
 const port = 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
